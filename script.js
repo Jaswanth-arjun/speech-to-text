46777791a1fdac626b2c7cee83c4972240c31f2a
Jaswanth-arjun <138548908+Jaswanth-arjun@users.noreply.github.com>
2025-09-19 20:24:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,59 @@
 const resultElement = document.getElementById("result");
-    let recognition;
+let recognition;
 
-    function startConverting() {
-      if ("webkitSpeechRecognition" in window) {
-        recognition = new webkitSpeechRecognition();
-        setupRecognition(recognition);
-        recognition.start();
-      } else {
-        alert("Speech Recognition not supported in this browser.");
-      }
+function startConverting() {
+  if ("webkitSpeechRecognition" in window) {
+    recognition = new webkitSpeechRecognition();
+    setupRecognition(recognition);
+    recognition.start();
+  } else {
+    alert("Speech Recognition not supported in this browser.");
+  }
+}
+
+function setupRecognition(recognition) {
+  recognition.continuous = true;
+  recognition.interimResults = true;
+  recognition.lang = "en-US";
+
+  recognition.onresult = function (event) {
+    const { finalTranscript, interimTranscript } = processResult(event.results);
+
+    // ✅ Better alignment and readability
+    resultElement.innerHTML =
+      `<div style="text-align:left; line-height:1.6; white-space:pre-wrap; word-wrap:break-word;">
+        ${finalTranscript}<br><i style="color:gray;">${interimTranscript}</i>
+      </div>`;
+  };
+
+  recognition.onerror = function (event) {
+    console.error("Speech recognition error:", event.error);
+  };
+
+  recognition.onend = function () {
+    console.log("Speech recognition stopped.");
+  };
+}
+
+function processResult(results) {
+  let finalTranscript = "";
+  let interimTranscript = "";
+
+  for (let i = 0; i < results.length; i++) {
+    let transcript = results[i][0].transcript;
+    transcript = transcript.replace(/\n/g, "<br>");
+    if (results[i].isFinal) {
+      finalTranscript += transcript + " "; // space between sentences
+    } else {
+      interimTranscript += transcript;
     }
+  }
 
-    function setupRecognition(recognition) {
-      recognition.continuous = true;
-      recognition.interimResults = true;
-      recognition.lang = "en-US";
+  return { finalTranscript, interimTranscript };
+}
 
-      recognition.onresult = function(event) {
-        const { finalTranscript, interimTranscript } = processResult(event.results);
-        resultElement.innerHTML = finalTranscript + "<i>" + interimTranscript + "</i>";
-      };
-
-      recognition.onerror = function(event) {
-        console.error("Speech recognition error:", event.error);
-      };
-
-      recognition.onend = function() {
-        console.log("Speech recognition stopped.");
-      };
-    }
-
-    function processResult(results) {
-      let finalTranscript = '';
-      let interimTranscript = '';
-
-      for (let i = 0; i < results.length; i++) {
-        let transcript = results[i][0].transcript;
-        transcript = transcript.replace(/\n/g, "<br>");
-        if (results[i].isFinal) {
-          finalTranscript += transcript;
-        } else {
-          interimTranscript += transcript;
-        }
-      }
-
-      return { finalTranscript, interimTranscript };
-    }
-
-    function stopConverting() {
-      if (recognition) {
-        recognition.stop();
-      }
-    }
+function stopConverting() {
+  if (recognition) {
+    recognition.stop();
+  }
+}
